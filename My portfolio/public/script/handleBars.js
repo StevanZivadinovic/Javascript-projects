83239100a84549f82ecffb4ee553b40921b93152
StevanZivadinovic <stevanzivadinovic@outlook.com>
2024-04-22 14:40:58 +0200

    update

--- My portfolio/public/script/handleBars.js
+++ My portfolio/public/script/handleBars.js
@@ -3,9 +3,17 @@ var template = Handlebars.compile(source);
 var context = {
   list: [
     {
+      nameProject: "Travel-Map app",
+      technologies: "NodeJS & React.js",
+      address:"https://mappin-fe.onrender.com/",
+      code: "https://github.com/StevanZivadinovic/mappin",
+      image: "./img/map_marker_app.png",
+    },
+    {
       nameProject: "Express.js website",
       technologies: "NodeJS & Express.js",
       address: "https://github.com/StevanZivadinovic/React.js-and-Vue.js/tree/main/node_simple_blog",
+      code:"https://github.com/StevanZivadinovic/React.js-and-Vue.js/tree/main/node_simple_blog",
       image: "./img/bloge_node_image.png",
     },
     {
@@ -18,6 +26,7 @@ var context = {
       nameProject: "Tax calculator",
       technologies: "ReactJS & Tailwind",
       address: "https://salestrakker-app.firebaseapp.com/",
+      code:"https://github.com/StevanZivadinovic/React.js-and-Vue.js/tree/main/Salestrakker",
       image: "./img/Tax_calculator.png",
     },
     {
@@ -30,66 +39,77 @@ var context = {
       nameProject: "Netflix clone",
       technologies: "HTML & Tailwind CSS & React & Typescript",
       address: "https://netflix-clone-website-3bc7f.web.app/",
+      code:"https://github.com/StevanZivadinovic/React.js-and-Vue.js/tree/main/netflix-clone-reactjs",
       image: "./img/netflix_clone.png",
     },
     {
       nameProject: "Website Graxpo",
       technologies: "HTML & SASS & javaScript & masonry plugin",
       address: "https://graxpo-6c872.web.app/",
+      code:"https://github.com/StevanZivadinovic/Ingsoftware-internship/tree/main/Graxpo",
       image: "./img/Graxpo.png",
     },
     {
       nameProject: "Website Birra",
       technologies: "HTML & SASS & javaScript & Bootstrap",
       address: "https://brewery-website-34605.web.app/",
+      code:"https://github.com/StevanZivadinovic/Ingsoftware-internship/tree/main/Birra",
       image: "./img/Birra.png",
     },
     {
       nameProject: "Website Countries presentation app",
       technologies: "ReactJS & TypeScript",
       address: "https://countries-presentation-a-ca18f.web.app/",
+      code:"https://github.com/StevanZivadinovic/Ingsoftware-internship/tree/main/countries-presentation-app",
       image: "./img/CountriesPresentation.png",
     },
     {
       nameProject: "Pravoslavne ikone",
       technologies: "ReactJS project",
       address: "https://pravoslavne-ikone-new.web.app/",
+      code:"https://github.com/StevanZivadinovic/React.js-and-Vue.js/tree/main/pravoslavne-ikone",
       image: "./img/pravoslavne-ikone.png",
     },
     {
       nameProject: "Asymmetry website",
       technologies: "HTML & SASS project",
       address: "https://asymmetry-website.web.app/",
+      code:"https://github.com/StevanZivadinovic/JavaScript-2nd/tree/main/Asymmetry%20website",
       image: "./img/Asymmetry.png",
     },
     {
       nameProject: "Prime hiring",
       technologies: "HTML & SASS & javaScript project",
       address: "https://prime-hiring-3b462.web.app/",
+      code:"https://github.com/StevanZivadinovic/JavaScript-2nd/tree/main/Prime%20Hiring",
       image: "./img/primeHiring.png",
     },
     {
       nameProject: "Rent a car",
       technologies: "JavaScript project",
       address: "https://rent-a-car-ddd10.web.app/",
+      code:"https://github.com/StevanZivadinovic/Javascript-projects/tree/master/Rent-a-car",
       image: "./img/rent-a-car.png",
     },
     {
       nameProject: "Natours website",
       technologies: "HTML & SASS project",
       address: "https://natours-e9cff.web.app/",
+      code:"https://github.com/StevanZivadinovic/JavaScript-2nd/tree/main/Noctour%20website",
       image: "./img/Natours.png",
     },
     {
       nameProject: "CryptoTrack App",
       technologies: "JavaScript project",
       address: "https://criptotrackapp.web.app",
+      code:"https://github.com/StevanZivadinovic/JavaScript-2nd/tree/main/CriptoTrackApp",
       image: "./img/CryptoTrackApp.png",
     },
     {
       nameProject: "Japanese watch",
       technologies: "JavaScript project",
       address: "https://japanese-watch.web.app/",
+      code:"https://github.com/StevanZivadinovic/Javascript-projects/tree/master/Japanese%20watch",
       image: "./img/japanese-watch.png",
     },
     {
